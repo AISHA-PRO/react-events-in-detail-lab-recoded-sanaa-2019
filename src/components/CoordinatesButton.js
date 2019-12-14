4af1dc603a77cbd7997230e57f6a79f5641cd4d5
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-14 09:17:34 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 export default class CoordinatesButton extends Component{
  getCoordinates = (e)=>{
    ler coordArr= [e.clientX, e.clientY]
+   
  }
 
   render(){
